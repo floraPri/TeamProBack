face8f40129d69014f941c6c1a1501c6ed290c50
KMJ <iris88819@naver.com>
2023-10-23 16:12:41 +0900

    20231023 commit

---
+++ src/main/java/com/np/wearound/dto/FeedDTO.java
@@ -0,0 +1,30 @@
+package com.np.wearound.dto;
+/**
+ * 피드 게시물에 대한 DTO
+ */
+import java.sql.Timestamp;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class FeedDTO {
+	private int feedcode;        		//NUMBER(10) PRIMARY KEY,
+	private String feedtitle;       	//   VARCHAR2(100) NOT NULL,
+	private String feedcontent;     	//CLOB NOT NULL,
+	private String feedimg;         	//VARCHAR2(100) NOT NULL,
+	private Timestamp feedregdate;		//TIMESTAMP DEFAULT sysdate,
+	private int userno;          		//NUMBER(10),               ---- 작성자 userno
+	private int comment_cnt;     		//   NUMBER(10) DEFAULT 0,
+	private String show;            	//  CHAR(3) DEFAULT 'Y' NOT NULL,
+	private String userid;				// 유저 아이디
+	
+}
+
+
---
+++ src/main/java/com/np/wearound/entities/Feed.java
@@ -0,0 +1,45 @@
+package com.np.wearound.entities;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@SequenceGenerator(
+		name = "SEQ_GENERATOR",
+		sequenceName = "feedcode_seq",
+		allocationSize = 1
+)
+@Entity
+@DynamicInsert
+@Table(name="feed_tbl")
+@Builder
+@Data
+public class Feed {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="SEQ_GENERATOR")
+	private int feedcode;        //NUMBER(10) PRIMARY KEY,
+	private String feedtitle;    //VARCHAR2(100) NOT NULL,
+    private String feedcontent;  //     CLOB NOT NULL,
+    private String feedimg;      //          VARCHAR2(100) NOT NULL,
+    private Timestamp feedregdate;	//     TIMESTAMP DEFAULT sysdate,
+    private int userno;					//  NUMBER(10),               ---- 작성자 userno
+    private int comment_cnt;        //NUMBER(10) DEFAULT 0,
+    private String show;              //CHAR(3) DEFAULT 'Y' NOT NULL
+	
+}
